22386648e871e67fea2912c6b9bd18b83920f3da
JoseOnniDev <josemarulanda.onni@gmail.com>
2021-10-25 20:55:50 -0500

    Cambios en la configuración inicial

--- src/config/default.js
+++ src/config/default.js
@@ -11,5 +11,20 @@ module.exports={
         password: process.env.DB_PASSWORD,
         ssl: { rejectUnauthorized: false },
         port: process.env.DB_PORT
-    }
+    },
+    TWILIO:{
+        id:process.env.TWILIO_ID,
+        token:process.env.TWILIO_TOKEN,
+        number : process.env.TWILIO_NUMBER
+
+    },
+    EMAIL:{
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL_USER, // generated ethereal user
+          pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+        },
+      }
 }
